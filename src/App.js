1ce28a2778334c392bfe8d75ae0f081a349a5663
Beth Spencer <beth.spencer@student.labette.edu>
2021-02-16 19:03:59 -0600

    added path for main and news

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
+import Navbar from "./components/Navbar";
 import './App.css';
+import Main from "./components/pages/Main";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import News from "./components/pages/News";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Main} />
+            <Route path="/news" component={News} />
+          </Switch>
+        </Router>
+      </>
   );
 }
 
