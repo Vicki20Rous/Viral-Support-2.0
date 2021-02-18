167d8e56bf2830c117c87419b6ffc3f97dcae83f
Beth Spencer <beth.spencer@student.labette.edu>
2021-02-18 15:48:08 -0600

    merged zipped file contents with my work

---
+++ src/components/utils/api.js
@@ -0,0 +1,15 @@
+import * as axios from 'axios';
+import {Component} from 'react';
+
+
+class newsApiCall extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            isFetching:false,
+            
+        }
+    }
+}
+
+export default newsApiCall;
