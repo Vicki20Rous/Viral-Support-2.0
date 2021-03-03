49800fa2acd9364b113c4d984556e57c9b9d9aae
GoddessVictoria <vic86rous@gmail.com>
2021-03-03 10:54:03 -0600

    updated server.js & controller files

--- servers/controllers/newsController.js
+++ servers/controllers/newsController.js
@@ -1,5 +1,17 @@
 const db = require("../models")
 
 module.exports = {
-    
-}
+    findAll: function(req, res) {
+        db.News
+        .find(req.query)
+        .sort({ date: -1 })
+        .then(dbModel => res.json(dbModel))
+        .catch(err=> res.status(422).json(err));
+    },
+    update: function(req, res) {
+        db.News
+        .findOneAndUpdate({_id: req.params.id }, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
+};
