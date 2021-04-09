3067398d96026c9508cc8a9c255fcf57be403dfb
GoddessVictoria <vic86rous@gmail.com>
2021-04-09 10:28:52 -0500

    added packages

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
@@ -24,31 +23,4 @@ export default function SignIn() {
       </div>
     </div>
   );
-=======
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-
-import './Join.css';
-
-export default function SignIn() {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-
-  return (
-    <div id="/Join" className="joinOuterContainer">
-      <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
-        <div>
-          <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
-        </div>
-        <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
-        </div>
-        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-          <button className={'button mt-20'} type="submit">Sign In</button>
-        </Link>
-      </div>
-    </div>
-  );
->>>>>>> 074641c6b53058cab6b8921750562c39f91e06d6
 }
